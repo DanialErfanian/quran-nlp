074e5f77b8d87f3b213cee2c85c60c1ec193ecf4
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-08-05 17:20:05 +0430

    Add quran english texts

--- quran_text/download.sh
+++ quran_text/download.sh
@@ -1,5 +1,21 @@
 for i in $(seq 1 114); do
-  export sorah_id=$(printf "%03d.txt" $i)
+  sorah_id=$(printf "%03d.txt" $i)
   echo "Downloading $sorah_id:"
   curl "https://www.quranful.com/text/ar-allah/$sorah_id" -o ./arabic/$sorah_id
+done
+
+for i in $(seq 1 114); do
+  sorah_id=$(printf "%03d.txt" $i)
+  echo "Downloading $sorah_id:"
+  FILE=./english/$sorah_id
+  if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+  else
+    curl "https://www.quranful.com/text/en-itani/$sorah_id" -o $FILE &
+  fi
+done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
 done
